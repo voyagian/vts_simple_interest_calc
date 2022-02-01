d4e7c72392ca44dbf388d401237814e82a055034
voyagian <98567340+voyagian@users.noreply.github.com>
2022-01-31 17:20:03 -1000

    updates in description
    - formatted form elements

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function compute()
 {
     p = document.getElementById("principal").value;
-    
+    p = document.getElementById("rate").value;
+    p = document.getElementById("years").value;
+
 }
-        
+function updateTextInput(val) {
+          document.getElementById('rate').value=val;
+        }
